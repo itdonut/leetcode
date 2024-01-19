f441c1bee10501acd04f4171c2c0ab28c8a75e1f
Serhii Paranchuk <serhiiparanchuk@gmail.com>
2024-01-19 20:07:26 +0200

    add changes to task (121)

--- Task_121.java
+++ Task_121.java
@@ -1,10 +1,5 @@
 package org.example.tasks;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class Task_121 {
     public int maxProfit(int[] prices) {
         int profit = 0;
